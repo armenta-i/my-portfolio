7cccade13106c1b8ca298a501cfb86833344a728
armenta-i <i_armenta@outlook.com>
2025-09-04 15:03:44 -0600

    Added unfinished contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { Email, GitHub, LinkedIn, MailOutline, PersonPinCircle, PersonPinCircleOutlined, Phone, PhoneOutlined } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import '../index.css';
 
 export default function Contact() {
     return (
@@ -10,24 +11,24 @@ export default function Contact() {
             className="h-full border-[0.1px] border-[#181926]"
             >
 
-                <div className="justify-center mb-50">
+                <div className="justify-center">
                     <div className="flex flex-column justify-center">
                         <h2 
                             className="text-center text-3xl md:text-7xl lg:text-7xl xl:text-7xl px-3 mb-4 md:mb-6 mt-15 md:mt-20 lg:mt-20 bg-gradient-to-r from-sky-500 to-fuchsia-600 inline-block text-transparent bg-clip-text"
-                            id="skills"
+                            id="contact"
                         >
                             Get In Contact
                         </h2>
                     </div>
                     {/* Header Underline */}
                     <div  className='w-24 md:w-30 h-1 mx-auto rounded-sm mb-10 bg-gradient-to-r from-sky-500 to-fuchsia-600 text-transparent'/>  
-                    <p className="text-center text-balance text-xl md:text-4xl lg:text-3xl text-[#8998ad] dark:text-[#8998ad] mb-10 lg:mb-25 mx-6 lg:mx-0">
+                    <p className="text-center text-balance text-xl md:text-4xl lg:text-3xl text-[#8998ad] dark:text-[#8998ad] mb-10 lg:mb-15 mx-6 lg:mx-0">
                         I'm always open to discussing new opportunities, collaborations, or just having a chat about technology.
                     </p>
                 </div>
 
                 {/* Main Info */}
-                <div className="md:flex px-4 md:px-40 md:gap-10 xl:px-50">
+                <div className="md:flex px-4 md:px-40 md:gap-10 xl:px-50 mb-20">
                     {/* Person Info */}
                     <div className="">
                         <h2 className="text-3xl md:text-4xl mb-5 text-wrap text-[#42a5f5]">
@@ -142,15 +143,80 @@ export default function Contact() {
                     {/* Form */}
                     <div
                         style={{ background: "var(--gradient-accent)", borderImage: "var(--gradient-accent-flipped)" }} 
-                        className="grow-1 rounded-lg w-full"
+                        className="grow-1 rounded-lg w-full h-fit"
                     >       
-                        <h2 className="text-2xl md:text-4xl mb-5 text-wrap text-[#42a5f5]">
+                        <h2 className="text-2xl md:text-4xl mb-5 text-wrap text-[#42a5f5] px-10 pt-5 pb-1">
                             Send a Message
                         </h2>
 
+                        <form>
+
+                            {/* <!-- To simplify the tutorial, the value is static. --> */}
+                            <div className="px-10">
+                            <input type="hidden" name="time" value="Mar 10 2025 08:46"/>
+                            <div className="md:flex">
+                                <div>
+                                {/* Name */}
+                                <label className="text-md">
+                                    Name
+                                </label><br/>
+                                <input 
+                                    type="text" 
+                                    name="name" 
+                                    required
+                                    className="bg-black my-2 mx-1 w-full rounded-lg py-1 px-3"
+                                    />
+                                </div>
+                            <div className="flex flex-col">
+                            {/* Email */}
+                            <label className="text-md text-white">
+                                Email
+                            </label><br/>
+                            </div>
+                                <input 
+                                    type="email" 
+                                    name="email" 
+                                    required
+                                    className="bg-black my-2 mx-1 w-full rounded-lg py-1 px-3"
+                                    />
+                            </div>
+                            {/* Subject */}
+                            <label className="text-md">
+                                Subject
+                            </label><br/>
+                                <input
+                                    type="text"
+                                    name="subject" 
+                                    required
+                                    className="bg-gray-900 mx-1 w-full rounded-lg py-1 px-3"
+                                />                            
+                                {/* Message */}
+                            <label>Message</label><br/>
+                            <textarea 
+                                name="message" 
+                                required
+                                className="bg-(--gradient-accent-flipped) my-2 mx-1 w-full h-30 rounded-lg md:rounded-bl-none py-1 px-3 resize-none"
+                                // background: "var(--gradient-accent)"
+                            >
+                            </textarea>
+                            {/* Submit Button */}
+                            <Button
+                                sx={{
+                                    textTransform: "none",
+                                }}
+                                component="label"
+                                role={undefined}
+                                variant="outlined"
+                                tabIndex={-1}
+                                // startIcon={<GitHub />}
+                                className="bg-[#181a1b] hover:bg-gradient-to-br hover:from-blue-700/20 hover:to-blue-700/20 border-1 border-[#42a5f5] w-full"
+                                >
+                            <input type="submit" value={"Submit Message"} className="py-2"/>
+                            </Button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-
             </section>
         </>
     )
